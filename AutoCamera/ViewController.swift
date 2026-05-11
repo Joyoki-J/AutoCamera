8dd7cbd6b74c51047da084c6415654ad30d93ad8
Shiqi Jiang <shiqi.jiang@sbitech.com>
2026-05-11 17:36:13 +0800

    first commit

---
+++ AutoCamera/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoCamera
+//
+//  Created by shiqi.jiang on 2026/5/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
